6a94d04154567361f5d05f837c845eee75274cbe
xkan-32 <121345057+xkan-32@users.noreply.github.com>
2023-03-03 20:24:03 +0900

    構文修正

--- serverspec/spec/3.230.175.24/sample_spec.rb
+++ serverspec/spec/3.230.175.24/sample_spec.rb
@@ -10,7 +10,7 @@ describe command("ps aux | grep unicorn" ) do
   its(:stdout) { should contain("unicorn master") }
 end
 
-alb_dns=ENV['RDS_DNS']
+alb_dns=ENV['ALB_DNS']
 
 describe command('curl #{alb_dns}/ -o /dev/null -w "%{http_code}\n" -s') do
 its(:stdout) { should match /^200$/ }
