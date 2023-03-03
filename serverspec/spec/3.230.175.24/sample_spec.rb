ff7d7d267b35449e8f432c71293d4e02643b5901
xkan-32 <121345057+xkan-32@users.noreply.github.com>
2023-03-03 18:44:08 +0900

    serverspec追加

--- serverspec/spec/3.230.175.24/sample_spec.rb
+++ serverspec/spec/3.230.175.24/sample_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
-listen_port = 8080
+listen_port = 80
 
-describe package('nginx') do
-  it { should be_installed }
+describe service('nginx') do
+  it { should be_running }
 end
 
-describe port(listen_port) do
-  it { should be_listening }
+describe command("ps aux | grep unicorn" ) do
+  its(:stdout) { should contain("unicorn master") }
 end
 
-describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
-  its(:stdout) { should match /^200$/ }
+alb_dns=ENV['RDS_DNS']
+
+describe command('curl #{alb_dns} -o /dev/null -w "%{http_code}\n" -s') do
+its(:stdout) { should match /^200$/ }
 end
